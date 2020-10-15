24b669370d07836a543e62dd59be68ba50376c74
Reward Khe <rewardkhe@outlook.com>
2020-10-16 00:08:01 +1100

    js file added.

---
+++ script.js
@@ -0,0 +1,10 @@
+function lsubmit() {
+    var element = document.body;
+    element.classList.toggle("l-submit");
+    window.open("https://memegenerator.net/img/instances/49889828/niggggaaaaaa-did-you-hit-that.jpg","_self")
+ }
+
+function nightmode() {
+    var element = document.body;
+    element.classList.toggle("work-mode");
+ }
