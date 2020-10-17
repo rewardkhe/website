26a94df0481bfba4349cac2505d3aabec33ce938
Reward Khe <rewardkhe@outlook.com>
2020-10-18 09:16:50 +1100

    fixed login and workmode button.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function lsubmit() {
-    var element = document.body;
-    element.classList.toggle("l-submit");
-    window.open("https://memegenerator.net/img/instances/49889828/niggggaaaaaa-did-you-hit-that.jpg","_self")
- }
 
 function nightmode() {
     var element = document.body;
     element.classList.toggle("work-mode");
+    element.classList.toggle("footer-black");
  }
