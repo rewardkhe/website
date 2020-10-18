5d879ca6d9a8806cf426b09fbb87ec85ce00cd8a
Reward Khe <rewardkhe@outlook.com>
2020-10-18 12:14:02 +1100

    background image.

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function nightmode() {
     var element = document.body;
     element.classList.toggle("work-mode");
     element.classList.toggle("footer-black");
+    
+    element.classList.toggle("footer-black");
  }
