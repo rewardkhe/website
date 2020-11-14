cea1f5bc8bc2f1649717c2de75f40f21247debb9
Reward Khe <rewardkhe@outlook.com>
2020-11-15 09:06:54 +1100

    fixed minor link errors, added work & sub work portfolios page.

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ function nightmode() {
     var element = document.body;
     element.classList.toggle("work-mode");
     element.classList.toggle("footer-black");
-    
     element.classList.toggle("footer-black");
  }
