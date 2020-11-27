80c7e6549528ffcdf6ceb168c0de1f00807642b9
Reward Khe <rewardkhe@outlook.com>
2020-11-27 12:54:43 +1100

    login windows fixed. or your changes. Lines starting

--- index.php
+++ index.php
@@ -1,44 +1,13 @@
 <?php
 
+$logintext = $_POST['logintext'];
 
-
-
-$host = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbname = "logindb";
-
-$logintext = $_POST['logintext']
-
-
-
-$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname)
-
-
-
-if (!empty($_POST['mainloginform'])) 
+if ($logintext == "admin" )
 {
-   
-   if $logintext = login (login_name)
-   {
-
-      echo "code matched"
-   }
-   else
-   {
-      echo "code required";
-      die(); 
-   }
+ header('Location: home.html');
 }
-
-
-
-
-
-
-     
-
-
-
-
+else{
+    echo '<script> alert("Invalid code Homie") </script>';
+    header('Location: index.html');
+}
 ?>
