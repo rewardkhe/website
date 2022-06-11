3f5b5ca861737038edeb2a2c38111b700bd0a53c
Reward Khe <rewardkhe@outlook.com>
2022-06-11 16:19:44 +1000

    fixed typos
    ate with 'origin/main'.

--- index.php
+++ index.php
@@ -20,14 +20,14 @@
 <main>
     <div class="intro">
         <div class="secintro" >
-            <h3> Hi, I'm a current student of  
+            <h3> Hi, I'm a recent grad of  
                 <a href="https://www.newcastle.edu.au/study/undergraduate?utm_campaign=eni-uon-undergrad-intake-cm-ugd&gclid=EAIaIQobChMIndfk97Tw9wIVyxErCh02wgR1EAAYAiAAEgKi4PD_BwE&gclsrc=aw.ds">The University of Newcastle</a> 
                 graduating with 
                 <a href="https://www.newcastle.edu.au/degrees/bachelor-of-information-technology"> Bachelor of Information technology</a>
                 and Minor in 
                 <a href="https://www.newcastle.edu.au/degrees/bachelor-of-computer-science">Computer Science</a> 
             </h3>
-            <!-- <p> Here is My <a href=""> Resume</a> and, my <a href="">transcript. </a>   -->
+            <p> Here is My <a href=""> Resume</a> and, my <a href="">transcript. </a>  
              <h4> <br> Relevant Coursework</h4>
                 <ul>
                     <li>Data Structures</li>
@@ -44,7 +44,7 @@
             
            <h4>  <br>Achievements </h4>
             <li> Placed on Faculty Commendation List 2020 - Faculty of Engineering and Built Environment </li>
-            <li> </li>
+          
             
             
         </div>
@@ -55,25 +55,22 @@
             <h3>Here are some of the projects I've done in past couple of years during uni throughout the coursework and side projects.</h3> <br>
                 <ul>
                     <a href="https://www.google.com" target="_blank"> <li>Macro Budget analyser</li> 
-                    <p> This app can let you view all your expenses in weekly, bi weekly basis</p> </a>
+                    <p> This app can let you view all your expenses in weekly, fortnightly, quarterly and so on.</p> </a>
                     <a href="https://www.google.com" target="_blank"> <li>Sudoku Solver</li> 
-                    <p> Point your smart phone camera to your sudoku grids and it will fill up all the remaining numbers in seconds </p> </a>
+                    <p> Point your smartphone camera to your sudoku grids and it will fill up all the remaining numbers in seconds.</p> </a>
                     <a href="https://www.google.com" target="_blank"> <li>Compound Visualiser</li> 
-                    <p> This app can let you view all your expenses in weekly, bi weekly basis</p> </a>
+                    <p> Experience the power of compounding with inflation rates and other admin fees.</p> </a>
                 
                 </ul>
         
         </div>
 
 
-        <div class="sec_caro" >
+        <!-- <div class="sec_caro" >
 
         <h3>Technologies I've used </h3>
-        <br> <p> [ logo carousel with hyperlink] </p>
-        
-        
-
-        </div>
+        <br> <p>  </p>
+        </div> -->
     </div>
     <div class="contact">
         
