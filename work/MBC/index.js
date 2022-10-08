5b989dbe19d7d0be0d08997dd0f1cb6c051781ca
Reward Khe <rewardkhe@outlook.com>
2022-10-08 11:10:58 +1100

    react link modded

---
+++ work/MBC/index.js
@@ -0,0 +1,33 @@
+// var output = document.getElementById('out');
+// var y = document.getElementById("out").innerText;
+// var x = "x";
+
+
+// function RentWF()
+// {
+//     // output.innerHTML = ++y;
+//     console.log (x + " this is x");
+//     console.log (y + " this is y");
+// }
+
+function frequencyFunction() {
+    var x = document.getElementById("Frequency").value;
+    document.getElementById("total").innerHTML = "50";
+  }
+
+var input = document.getElementById("textinput");
+var inputData = input.innerText;
+function demo(){
+    document.getElementById("textoutput").innerText = inputData;
+    
+}
+
+
+// function multAction()
+// {
+//     output.innerHTML = Math.pow(y, 2);
+//     y = output.innerHTML;
+//     console.log (x + " this is x");
+//     console.log (y + " this is y");
+// }
+
